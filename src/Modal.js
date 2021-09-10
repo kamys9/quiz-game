b0c74fa97d2e975420329c3fa02096e2c7fe667a
Kamila <kamila.yassinova@gmail.com>
2021-09-10 09:55:52 +0200

    fav icon

--- src/Modal.js
+++ src/Modal.js
@@ -6,7 +6,7 @@ const Modal = () => {
     useGlobalContext()
   return <div className={`${isModalOpen ? 'modal-container isOpen' : 'modal-container'}`}>
     <div className='modal-content'>
-      <h2 className='congrats'>congrats!</h2>
+      <h2 className='congrats'>congratulations!</h2>
       {/* <p>You answered {((correct / questions.length) * 100).toFixed(0)}% of questions correctly</p> */}
       <p className='congrats'>You answered {correct} of {questions.length} questions correctly</p>
 
